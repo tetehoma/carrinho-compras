698f6c5e01a0fecfb979140c6e54f960bb27630f
tetehoma <tetehoma@gmail.com>
2025-11-08 05:55:07 +0900

    take quantify

--- js/app.js
+++ js/app.js
@@ -7,4 +7,9 @@ function adicionar() {
     //produt.options[produt.selectedIndex].text (put the value into variable)
     product = document.getElementById('produto');
     let nameProduct = product.options[product.selectedIndex].text;
+
+    //valueAsNumber put the value the type number in the variable let.
+    quant = document.getElementById('quantidade');
+    const quantProdut = quant.valueAsNumber;
+    
 }
